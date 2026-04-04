d6d5e519d4af2299a670221749a3272315234169
FIIN YTID <arifiinytid@gmail.com>
2026-04-04 18:20:35 +0800

    Update main.js

--- api/main.js
+++ api/main.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({
     gemini: process.env.GEMINI_API_KEY,
     claude: process.env.CLAUDE_API_KEY,
-    openai: process.env.OPENAI_API_KEY
+    openai: process.env.OPENAI_API_KEY,
+    grok: process.env.GROK_API_KEY
   });
 }
