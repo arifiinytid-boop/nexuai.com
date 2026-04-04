aded687030ef6c3f6f5d9b2dd032b9f437200706
FIIN YTID <arifiinytid@gmail.com>
2026-04-04 23:10:13 +0800

    Update main.js

--- api/main.js
+++ api/main.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
+    nexusai: process.env.NEXUSAI_API_KEY,
     gemini: process.env.GEMINI_API_KEY,
     claude: process.env.CLAUDE_API_KEY,
     openai: process.env.OPENAI_API_KEY,
