bb736379899bc3b7858c09706b420d44b13d8261
FIIN YTID <arifiinytid@gmail.com>
2026-05-04 19:38:04 +0800

    Update control.js

--- api/control.js
+++ api/control.js
@@ -2,7 +2,7 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 
 const TMP = '/tmp';
-export const REQUIRED_PLUGIN_VERSION = 'V1.1.95';
+export const REQUIRED_PLUGIN_VERSION = 'V1.1.96';
 export const WEB_VERSION = 'V10.5';
 
 // ─── SANITIZE ─────────────────────────────────────────────
